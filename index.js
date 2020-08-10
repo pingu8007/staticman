0af3923b270a4ba2adee04417e7ad3dd43a37e53
PinGu <pingu8007@gmail.com>
2020-08-10 09:47:15 +0000

    Supporting Google Cloud Functions
    Add dev-dependency `functions-framework` for local testing.
    Update minimum node version to 10.
    Move dependency `nodemon` to dev-dependency.
    Update scripts.

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-try {
-  const StaticmanAPI = require('./server')
-  const api = new StaticmanAPI()
-
-  api.start(port => {
-    console.log('Staticman API running on port', port)
-  })
-} catch (e) {
-  console.error(e)
-}
+const StaticmanAPI = require('./server')
+exports.function = new StaticmanAPI().server
